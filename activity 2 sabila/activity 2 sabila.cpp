14a0707e72102adc884cba9d3160b6644f19ebb9
FatihSholeh <FatihSholeh@LAPTOP-MK72SEFU>
2024-03-05 10:32:15 +0700

    Add project files.

---
+++ activity 2 sabila/activity 2 sabila.cpp
@@ -0,0 +1,23 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int bilangan;
+    srand(time(0));
+
+    bilangan = rand() % 10;
+
+    cout << "Bilangan awal =" << bilangan << endl;
+
+    if (bilangan >= 5) {
+        bilangan = 2 * bilangan;
+    }
+
+    cout << "Bilangan akhir =" << bilangan << endl;
+    return 0;
+}
+
