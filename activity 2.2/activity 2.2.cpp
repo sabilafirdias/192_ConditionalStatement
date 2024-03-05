26bd5de18ce76b62f9fd989b0687f1b44ae59646
sabilafiridas <sabilafirdias@gmail.com>
2024-03-05 15:15:10 +0700

    membandingkan dua bilangan

---
+++ activity 2.2/activity 2.2.cpp
@@ -0,0 +1,34 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nBilanganA;
+    int nBilanganB;
+    string status;
+
+    srand(time(0));
+
+    nBilanganA = rand() % 6;
+    nBilanganB = rand() % 2;
+
+    if (nBilanganA == nBilanganB) {
+        status = "Sama Besar";
+    }
+    else if (nBilanganA > nBilanganB) {
+        status = "Bilangan A lebih besar dari Bilangan B";
+    }
+    else {
+        status = "Bilangan A lebih kecil dari Bilangan B";
+    }
+
+    cout << "Bilangan Pertama adalah " << nBilanganA << endl;
+    cout << "Bilangan Kedua adalah " << nBilanganB << endl;
+    cout << "Status = " << status << endl;
+
+    return 0;
+}
+
+
